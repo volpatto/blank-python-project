314c40c9ded2fbe63363e0fa1f12f63d51b4d125
volpatto <volpatto@lncc.br>
2019-11-06 21:51:56 -0300

    Add modifications by pre-commit checkers and formatters

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -7,11 +7,8 @@ import mypackage.foo
 @pytest.mark.parametrize("dim", [3, 4, 5])
 def test_a_function(dim):
 
-    output = mypackage.foo.a_function(
-        x=0.0,
-        dim=dim
-    )
+    output = mypackage.foo.a_function(x=0.0, dim=dim)
 
-    pass_conditions = (output == pytest.approx(np.ones(dim)))
+    pass_conditions = output == pytest.approx(np.ones(dim))
     if not pass_conditions:
         raise AssertionError()
